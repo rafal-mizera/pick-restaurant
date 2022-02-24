4a61b1c91f7315981e17958308ac47f43e5a39b3
rafal_mizera <rafalmizera11@gmail.com>
2022-02-24 17:17:38 +0100

    updated setting.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rkf5%k5h$z+29b-!(8f@(nhs9j7p(cj2nxx8wvxtr$^9-)pyzu'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pick-your-restaurant.herokuapp.com','127.0.0.1']
 
 
 # Application definition
